8108e96347c20f6a89d9b47e5dfa8128c5851d6f
Hari Haran N <110599490+Hareesvar7@users.noreply.github.com>
2024-10-16 21:30:32 +0530

    Update VisualizationCommand.js

--- VisualizationCommand.js
+++ VisualizationCommand.js
@@ -3,46 +3,42 @@
 const vscode = require('vscode');
 const VisualizationService = require('../services/VisualizationService');
 
-module.exports = async function visualizePolicies(context) {
+async function visualizePolicies(context) {
     const disposable = vscode.commands.registerCommand('extension.visualizePolicies', async function () {
         // Prompt user to select a .rego file
-        const regoFileUri = await vscode.window.showOpenDialog({
+        const fileUri = await vscode.window.showOpenDialog({
             canSelectMany: false,
             filters: {
                 'Rego Files': ['rego']
             }
         });
 
-        if (!regoFileUri || regoFileUri.length === 0) {
-            vscode.window.showErrorMessage('No file selected');
+        if (!fileUri || fileUri.length === 0) {
+            vscode.window.showErrorMessage('No file selected.');
             return;
         }
 
-        const regoFilePath = regoFileUri[0].fsPath;
-
-        try {
-            const policies = await VisualizationService.getPoliciesFromFile(regoFilePath);
-            if (!policies) {
-                vscode.window.showErrorMessage('No policies found to visualize.');
-                return;
-            }
-
-            // Open Webview for visualization
-            const panel = vscode.window.createWebviewPanel(
-                'policyVisualization',
-                'Policy Visualization',
-                vscode.ViewColumn.One,
-                {
-                    enableScripts: true
-                }
-            );
-
-            // Set the HTML content for the webview
-            panel.webview.html = VisualizationService.getVisualizationHTML(policies);
-        } catch (error) {
-            vscode.window.showErrorMessage(`Error: ${error.message}`);
+        const policies = await VisualizationService.getPolicies(fileUri[0].fsPath);
+        if (!policies) {
+            vscode.window.showErrorMessage('No policies found to visualize.');
+            return;
         }
+
+        // Open Webview for visualization
+        const panel = vscode.window.createWebviewPanel(
+            'policyVisualization',
+            'Policy Visualization',
+            vscode.ViewColumn.One,
+            {
+                enableScripts: true
+            }
+        );
+
+        // Set the HTML content for the webview
+        panel.webview.html = VisualizationService.getVisualizationHTML(policies);
     });
 
     context.subscriptions.push(disposable);
-};
+}
+
+module.exports = visualizePolicies;
