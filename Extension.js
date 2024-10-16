99683efa3198e9c02bccd8aaf334c616d9c62671
Hari Haran N <110599490+Hareesvar7@users.noreply.github.com>
2024-10-16 20:37:02 +0530

    Update Extension.js

--- Extension.js
+++ Extension.js
@@ -2,36 +2,24 @@ const vscode = require('vscode');
 const { showCloudServiceSelection } = require('./commands/cloudServiceSelection');
 const { validateOPA } = require('./commands/Opavalidate');
 const { aiAssist } = require('./commands/Aiassist');
-const { visualizePolicies } = require('./commands/VisualizationCommand');
+const { visualizePolicies } = require('./commands/visualizePolicies'); // Ensure this file exists
 
 function activate(context) {
     console.log('Extension "PolicyPilot" is now active!');
 
-    // Register cloud service selection command
-    let cloudServiceSelectionCmd = vscode.commands.registerCommand('policyPilot.showCloudServiceSelection', function () {
-        showCloudServiceSelection(context);
-    });
-
-    // Register OPA validation command
-    let validateOPACmd = vscode.commands.registerCommand('policyPilot.validateOPA', function () {
-        validateOPA(context);
-    });
-
-    // Register AI Assist command
-    let aiAssistCmd = vscode.commands.registerCommand('policyPilot.aiAssist', function () {
-        aiAssist(context);
-    });
-
-    // Register Policy Visualization command
-    let visualizePoliciesCmd = vscode.commands.registerCommand('policyPilot.visualizePolicies', function () {
-        visualizePolicies(context);
-    });
-
-    // Add all commands to context subscriptions
-    context.subscriptions.push(cloudServiceSelectionCmd);
-    context.subscriptions.push(validateOPACmd);
-    context.subscriptions.push(aiAssistCmd);
-    context.subscriptions.push(visualizePoliciesCmd);
+    // Register commands
+    context.subscriptions.push(
+        vscode.commands.registerCommand('policyPilot.showCloudServiceSelection', showCloudServiceSelection)
+    );
+    context.subscriptions.push(
+        vscode.commands.registerCommand('policyPilot.validateOPA', validateOPA)
+    );
+    context.subscriptions.push(
+        vscode.commands.registerCommand('policyPilot.aiAssist', aiAssist)
+    );
+    context.subscriptions.push(
+        vscode.commands.registerCommand('policyPilot.visualizePolicies', visualizePolicies)
+    );
 }
 
 function deactivate() {}
