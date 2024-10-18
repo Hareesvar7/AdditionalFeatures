b1f20fac4cfc772d1230a7f6e7f0b54d180a6cec
Hari Haran N <110599490+Hareesvar7@users.noreply.github.com>
2024-10-18 12:57:51 +0530

    Update Extension.js

--- Extension.js
+++ Extension.js
@@ -1,27 +1,33 @@
-// src/Extension.js
-
 const vscode = require('vscode');
 const registerAutoCompletion = require('./commands/AutoCompleteCommand');
+const LintPolicy = require('./commands/LintPolicy');
+const ValidateOPA = require('./commands/ValidateOPA'); // Assuming you have this command
+const ShowCloudServiceSelection = require('./commands/ShowCloudServiceSelection'); // Assuming you have this command
+const AiAssist = require('./commands/AiAssist'); // Assuming you have this command
+const VisualizePolicies = require('./commands/VisualizePolicies'); // Assuming you have this command
 
 function activate(context) {
-    // Register the auto-completion command for AWS S3 Rego policies
-    context.subscriptions.push(
-        vscode.commands.registerCommand('extension.autoCompleteS3Policies', async () => {
-            // Open a new document with a Rego file extension
-            const newDocument = await vscode.workspace.openTextDocument({ language: 'rego' });
-            await vscode.window.showTextDocument(newDocument);
-
-            // Automatically suggest the package when the document is opened
-            const editor = vscode.window.activeTextEditor;
-            if (editor) {
-                editor.edit(editBuilder => {
-                    editBuilder.insert(new vscode.Position(0, 0), 'package aws.s3.policies\n');
-                });
+    // Register the lint policy command
+    context.subscriptions.push(vscode.commands.registerCommand('extension.lintPolicy', async () => {
+        const fileUri = await vscode.window.showOpenDialog({
+            canSelectMany: false,
+            filters: {
+                'Rego Files': ['rego']
             }
-        })
-    );
+        });
 
-    // Register auto-completion feature
+        if (fileUri && fileUri[0]) {
+            await LintPolicy.execute(fileUri[0].fsPath);
+        }
+    }));
+
+    // Register the other commands
+    context.subscriptions.push(vscode.commands.registerCommand('extension.showCloudServiceSelection', ShowCloudServiceSelection));
+    context.subscriptions.push(vscode.commands.registerCommand('extension.validateOPA', ValidateOPA));
+    context.subscriptions.push(vscode.commands.registerCommand('extension.aiAssist', AiAssist));
+    context.subscriptions.push(vscode.commands.registerCommand('extension.visualizePolicies', VisualizePolicies));
+
+    // Register the auto-completion feature
     registerAutoCompletion(context);
 }
 
