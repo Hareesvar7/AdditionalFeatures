a8eff901c8d58bc939435e5787fb5fa528141f2e
Hari Haran N <110599490+Hareesvar7@users.noreply.github.com>
2024-10-18 23:22:02 +0530

    Update Extension.js

--- Extension.js
+++ Extension.js
@@ -1,9 +1,9 @@
 const vscode = require('vscode');
-const versionPolicy = require('./commands/PolicyVersioningCommand');
+const policyVersionCommand = require('./commands/PolicyVersionCommand');
 
 function activate(context) {
-    // Register the version policy command
-    let disposable = vscode.commands.registerCommand('extension.versionPolicy', versionPolicy);
+    // Register the policy version command
+    let disposable = vscode.commands.registerCommand('extension.policyVersion', policyVersionCommand);
     context.subscriptions.push(disposable);
 
     // Other existing commands can be registered here
