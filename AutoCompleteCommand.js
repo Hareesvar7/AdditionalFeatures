b68340526fb8e675801bd631e332a296dbeddb5b
Hari Haran N <110599490+Hareesvar7@users.noreply.github.com>
2024-10-18 12:58:24 +0530

    Update AutoCompleteCommand.js

--- AutoCompleteCommand.js
+++ AutoCompleteCommand.js
@@ -16,56 +16,36 @@ async function registerAutoCompletion(context) {
                     // 1. Enforce S3 Access Points in VPC Only
                     const s3AccessPointInVPC = new vscode.CompletionItem('Enforce S3 Access Points in VPC Only', vscode.CompletionItemKind.Snippet);
                     s3AccessPointInVPC.insertText = new vscode.SnippetString(
-                        `deny[msg] {
-    resource := input.resource_changes[_]
-    resource.type == "aws_s3_access_point"
-    not resource.change.after.vpc_configuration
-    msg = sprintf("S3 Access Point '%s' must be configured in a VPC", [resource.change.after.name])
-}`);
+                        `deny[msg] {\n    resource := input.resource_changes[_]\n    resource.type == "aws_s3_access_point"\n    not resource.change.after.vpc_configuration\n    msg = sprintf("S3 Access Point '%s' must be configured in a VPC", [resource.change.after.name])\n}`
+                    );
                     snippets.push(s3AccessPointInVPC);
 
                     // 2. Enforce Public Access Blocks on S3 Access Points
                     const publicAccessBlocks = new vscode.CompletionItem('Enforce Public Access Blocks on S3 Access Points', vscode.CompletionItemKind.Snippet);
                     publicAccessBlocks.insertText = new vscode.SnippetString(
-                        `deny[msg] {
-    resource := input.resource_changes[_]
-    resource.type == "aws_s3_access_point"
-    not resource.change.after.public_access_block
-    msg = sprintf("S3 Access Point '%s' must have public access blocks enabled", [resource.change.after.name])
-}`);
+                        `deny[msg] {\n    resource := input.resource_changes[_]\n    resource.type == "aws_s3_access_point"\n    not resource.change.after.public_access_block\n    msg = sprintf("S3 Access Point '%s' must have public access blocks enabled", [resource.change.after.name])\n}`
+                    );
                     snippets.push(publicAccessBlocks);
 
                     // 3. Enforce Account-Level Public Access Blocks
                     const accountLevelPublicAccess = new vscode.CompletionItem('Enforce Account-Level Public Access Blocks', vscode.CompletionItemKind.Snippet);
                     accountLevelPublicAccess.insertText = new vscode.SnippetString(
-                        `deny[msg] {
-    resource := input.resource_changes[_]
-    resource.type == "aws_s3_bucket"
-    not resource.change.after.account_level_public_access_block
-    msg = sprintf("S3 bucket '%s' must have account-level public access blocks enabled", [resource.change.after.bucket])
-}`);
+                        `deny[msg] {\n    resource := input.resource_changes[_]\n    resource.type == "aws_s3_bucket"\n    not resource.change.after.account_level_public_access_block\n    msg = sprintf("S3 bucket '%s' must have account-level public access blocks enabled", [resource.change.after.bucket])\n}`
+                    );
                     snippets.push(accountLevelPublicAccess);
 
                     // 4. Prohibit ACLs on S3 Buckets
                     const prohibitACLs = new vscode.CompletionItem('Prohibit ACLs on S3 Buckets', vscode.CompletionItemKind.Snippet);
                     prohibitACLs.insertText = new vscode.SnippetString(
-                        `deny[msg] {
-    resource := input.resource_changes[_]
-    resource.type == "aws_s3_bucket"
-    resource.change.after.acl != "private"
-    msg = sprintf("S3 bucket '%s' must not use ACLs", [resource.change.after.bucket])
-}`);
+                        `deny[msg] {\n    resource := input.resource_changes[_]\n    resource.type == "aws_s3_bucket"\n    resource.change.after.acl != "private"\n    msg = sprintf("S3 bucket '%s' must not use ACLs", [resource.change.after.bucket])\n}`
+                    );
                     snippets.push(prohibitACLs);
 
                     // 5. Prohibit Blacklisted Actions on S3 Buckets
                     const blacklistedActions = new vscode.CompletionItem('Prohibit Blacklisted Actions on S3 Buckets', vscode.CompletionItemKind.Snippet);
                     blacklistedActions.insertText = new vscode.SnippetString(
-                        `deny[msg] {
-    resource := input.resource_changes[_]
-    resource.type == "aws_s3_bucket"
-    action := resource.change.after.blacklisted_actions[_]
-    msg = sprintf("S3 bucket '%s' contains blacklisted action '%s'", [resource.change.after.bucket, action])
-}`);
+                        `deny[msg] {\n    resource := input.resource_changes[_]\n    resource.type == "aws_s3_bucket"\n    action := resource.change.after.blacklisted_actions[_]\n    msg = sprintf("S3 bucket '%s' contains blacklisted action '%s'", [resource.change.after.bucket, action])\n}`
+                    );
                     snippets.push(blacklistedActions);
 
                     return snippets;
